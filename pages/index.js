e58ac80f9e694c8445acc38960e7a248837971ad
Saad Khoudmi <khoudmi@gmail.com>
2019-11-15 21:39:08 +0200

    VKUI connect

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
-function Home() {
-  return <div>My name is Gan, Gan Lagarfeld</div>;
+import { View, Panel, PanelHeader, FormLayout, File } from "@vkontakte/vkui";
+import IconCamera24 from "@vkontakte/icons/dist/24/camera";
+import IconDocument24 from "@vkontakte/icons/dist/24/document";
+import "@vkontakte/vkui/dist/vkui.css";
+
+function App() {
+  return (
+    <View activePanel="panel">
+      <Panel id="panel" theme="white">
+        <PanelHeader>File</PanelHeader>
+        <FormLayout>
+          <File top="Upload your photo" before={<IconCamera24 />} size="l">
+            Upload your photo
+          </File>
+          <File
+            top="Download Documents"
+            before={<IconDocument24 />}
+            size="xl"
+            level="secondary"
+          >
+            Download Documents
+          </File>
+        </FormLayout>
+      </Panel>
+    </View>
+  );
 }
 
-export default Home;
+export default App;
